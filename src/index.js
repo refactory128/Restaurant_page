b720f63bdd32fd103226c5ef510a024c6316ded5
WebWorflowSolutions99 <83876343+WebWorflowSolutions99@users.noreply.github.com>
2021-12-14 07:14:39 -0600

    add menu page functionality and images

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import './style.css';
 import Logo from './img/Logo.jpeg';
 import backgroundImageSrc from './img/Welcome.jpeg';
 
+import imgSrc1 from './img/EggBurger.jpeg';
+import imgSrc2 from './img/Sushi.jpeg';
+import imgSrc3 from './img/Spaghetti.jpeg';
+
+
 console.log("Hello Webpack");
 
 function navBar(){
@@ -41,18 +46,42 @@ function welcomePage(){
     page.appendChild(rightDiv);
 
     const h1 = document.createElement("h1");
-    h1.innerHTML = "WELCOME"
+    h1.innerHTML = "WELCOME";
     rightDiv.appendChild(h1);
 
     const p = document.createElement("p");
-    p.innerHTML = "Great Food, Good Times"
+    p.innerHTML = "Great Food, Good Times";
     rightDiv.appendChild(p);
 
     return page;
 }
 
 
+function addMenuItem(pictureSrc, title, description){
+    const item = document.createElement("div");
+    item.classList.add('menuItem');
+
+    const picture = document.createElement("img");
+    picture.src = pictureSrc;
+    item.appendChild(picture);
+
+    const h1 = document.createElement("h1");
+    h1.innerHTML = title;
+    item.appendChild(h1);
+
+    const p = document.createElement("p");
+    p.innerHTML = description;
+    item.appendChild(p);
+
+    return item;
+}
+
+
 const content = document.querySelector("#content");
 
 content.appendChild(navBar());
-content.appendChild(welcomePage());
+content.appendChild(addMenuItem(imgSrc1, "Egg Burger", "this scrumptions egg burger comes with fries"));
+content.appendChild(addMenuItem(imgSrc2, "Sushi", "this scrumptions Sushi comes with soysauce"));
+content.appendChild(addMenuItem(imgSrc3, "Spaghetti", "this scrumptions pasta comes with sauce"));
+
+//content.appendChild(welcomePage());
